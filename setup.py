12f6b82ac9e798d6530b8fdc5d1f2d17d0fab144
Dave Hershberger <hersh@willowgarage.com>
2012-09-20 13:39:34 -0700

    package.xml: added missing author and updated setup.py to Dirk's new tinier version

--- setup.py
+++ setup.py
@@ -1,34 +1,9 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-from setuptools import setup
-import sys
-from xml.etree.ElementTree import ElementTree
+from distutils.core import setup
+from catkin.package import parse_package_for_distutils
 
-try:
-    root = ElementTree(None, 'stack.xml')
-    version = root.findtext('version')
-except Exception as e:
-    print('Could not extract version from your stack.xml:\n%s' % e, file=sys.stderr)
-    sys.exit(1)
-
-setup(name = 'gencpp',
-      version = version,
-      packages = ['gencpp'],
-      package_dir = {'': 'src'},
-      install_requires = ['genmsg'],
-      scripts = ['scripts/gen_cpp.py'],
-      author = "Morgen Kjaergaard, Troy Straszheim, Josh Faust",
-      author_email = "straszheim@willowgarage.com",
-      url = "http://www.ros.org/wiki/gencpp",
-      download_url = "http://pr.willowgarage.com/downloads/gencpp/",
-      keywords = ["ROS"],
-      classifiers = [
-        "Programming Language :: Python",
-        "License :: OSI Approved :: BSD License" ],
-      description = "ROS msg/srv C++ generation",
-      long_description = """\
-Library and scripts for generating ROS message data structures in C++.
-""",
-      license = "BSD"
-      )
+d = parse_package_for_distutils()
+d['packages'] = ['gencpp']
+d['package_dir'] = {'': 'src'}
+setup(**d)
