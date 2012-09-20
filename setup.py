0bad9717585c6050659d08ad734b92151f88fad6
Dave Hershberger <hersh@willowgarage.com>
2012-09-20 14:21:37 -0700

    setup.py: added missing install_requires and scripts entries in dictionary (which I lost in previous commit).

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ from catkin.package import parse_package_for_distutils
 d = parse_package_for_distutils()
 d['packages'] = ['gencpp']
 d['package_dir'] = {'': 'src'}
+d['install_requires'] = ['genmsg']
+d['scripts'] = ['scripts/gencpp.py']
 setup(**d)
