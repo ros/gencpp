1a7666b03692899b69afc34814b3c0e599a01235
Dirk Thomas <dthomas@willowgarage.com>
2012-09-21 12:58:08 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from catkin.package import parse_package_for_distutils
+from catkin_pkg.package import parse_package_for_distutils
 
 d = parse_package_for_distutils()
 d['packages'] = ['gencpp']
 d['package_dir'] = {'': 'src'}
-d['install_requires'] = ['genmsg']
 d['scripts'] = ['scripts/gen_cpp.py']
+d['install_requires'] = ['genmsg']
+
 setup(**d)
